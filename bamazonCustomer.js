7c063f47c8d2f39d8d40496c80f524f93061f3fb
Leon Lin <leonlin310@gmail.com>
2019-05-28 23:59:36 -0500

    Working orders! Now to deduct quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,20 +15,70 @@ connection.connect(function (err) {
     console.log("Connected as ID: ", connection.threadId);
     createItem();
     //Step 1: create an inquirer prompt to post or bid
-    // postOrBid();
+    showInventory()
 })
 
-function createItem(){
-var query = connection.query("INSERT INTO products SET ?", 
-    {
-        item_id: 11,
-        product_name: "tester",
-        department_name: "bootcamp",
-        price: 500,
-        stock_quantity: 200
-    },
-    function(err, res){
-        console.log("item added \n")
-    })
-
+function createItem() {
+    var query = connection.query("INSERT INTO products SET ?",
+        {
+            item_id: 11,
+            product_name: "tester",
+            department_name: "bootcamp",
+            price: 500,
+            stock_quantity: 200
+        },
+        function (err, res) {
+            console.log("item added \n")
+        })
 }
+
+function showInventory() {
+    connection.query("SELECT * FROM products", (err, res) => {
+        if (err) throw err;
+        console.log("Welcome to the Bamazon Store! Below is a list of available items for sale: \n")
+        console.table(res);
+        //chain to BUY function
+        buy();
+    })
+}
+
+//TODO: The app should then prompt users with two messages.
+//TODO: The first should ask them the ID of the product they would like to buy.
+//TODO: The second message should ask how many units of the product they would like to buy.
+function buy() {
+    connection.query("SELECT * FROM products", (err, res) => {
+        // console.log("This is res: \n", res)
+        inquirer.prompt([
+            {
+                name: "product_name",
+                message: "What would you like to purchase?",
+                type: "list",
+                choices: res.map(item => item.product_name)
+            },
+            {
+                name: "stock_quantity",
+                message: "How much would you like to buy?"
+            }])
+        .then(answers => {
+            // console.log("User choice answers: \n", answers)
+            let productChosen = res.find(item => item.product_name == answers.product_name)
+            // console.log("This is Product Chosen ========= \n", productChosen)
+            if (productChosen.stock_quantity >= answers.stock_quantity){
+                console.log("Your order has been processed. Would you like to purchase anything else? \n")
+                buy();
+            }
+
+            else {
+                console.log("There is insufficient quantity!")
+            }
+        })
+
+    })
+}
+
+// TODO: Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// TODO: This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
